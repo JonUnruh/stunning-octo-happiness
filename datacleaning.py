d9a49581fd1c96956dff411261ead90763efdcea
JonUnruh <tob.me@t-online.de>
2017-06-01 13:31:01 +0200

    Update datacleaning.py

--- datacleaning.py
+++ datacleaning.py
@@ -1,8 +1,3 @@
-"""
-Created on Tue May 30 14:38:49 2017
-
-@author: Olaf
-"""
 import pandas as pd
 df = pd.read_csv('american-election-tweets.csv', sep=';', encoding = 'iso8859-1')
 print (df.shape)
