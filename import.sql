cfdc703b0bddb3531229ff1c8a789d3998776b61
JonUnruh <tob.me@t-online.de>
2017-06-01 14:31:05 +0200

    Update import.sql

--- import.sql
+++ import.sql
@@ -1,3 +1,3 @@
-COPY tweet(tweet_id, handle text, inhalt text, ist_retweet,"ursprünglicher_autor", datum, antwort_auf, ist_zitat, anzahl_retweets, anzahl_likes) 
+COPY tweet(tweet_id, handle, inhalt, ist_retweet, "ursprünglicher_autor", datum, antwort_auf, ist_zitat, anzahl_retweets, anzahl_likes) 
 FROM 'C:\Program Files\PostgreSQL\9.6\data\amelec-filtered.csv'
 WITH DELIMITER ';' CSV HEADER;
