365db78a95306f72299e03ca23af611fc0f169a1
JonUnruh <tob.me@t-online.de>
2017-06-01 14:48:03 +0200

    Update datacleaning.py

--- datacleaning.py
+++ datacleaning.py
@@ -1,6 +1,6 @@
-import pandas as pd
+ import pandas as pd
 df = pd.read_csv('american-election-tweets.csv', sep=';', encoding = 'iso8859-1')
-df2 = df.drop(["source_url"], axis = 1)
-df3 = df2[df2.truncated != True]
-df3 = df3.drop(["truncated"], axis=1)
+df2 = df.drop(["source_url"], axis = 1) #entfernt Spalte "source_url"
+df3 = df2[df2.truncated != True]  #entfernt alle gekürzten Tweets
+df3 = df3.drop(["truncated"], axis=1) #entfernt Spalte "truncated"
 df3.to_csv('amelec-filtered.csv', sep=';', encoding = 'iso8859-1')
