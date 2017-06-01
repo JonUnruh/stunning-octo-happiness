1bd2f0d3fe74293f73274ee9c2cfe60ff3d2e49e
JonUnruh <tob.me@t-online.de>
2017-06-01 14:42:19 +0200

    Update datacleaning.py

--- datacleaning.py
+++ datacleaning.py
@@ -1,9 +1,6 @@
 import pandas as pd
 df = pd.read_csv('american-election-tweets.csv', sep=';', encoding = 'iso8859-1')
-print (df.shape)
 df2 = df.drop(["source_url"], axis = 1)
-print(df2.shape)
 df3 = df2[df2.truncated != True]
 df3 = df3.drop(["truncated"], axis=1)
-print(df3)
 df3.to_csv('amelec-filtered.csv', sep=';', encoding = 'iso8859-1')
